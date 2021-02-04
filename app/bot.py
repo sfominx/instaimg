db91817902894ca7eabcb65ff27e165efc6f64af
moocher <moocher@moocher.ru>
2021-02-04 13:05:44 +0300

    Linting, Telegram bot version updated
    Took 1 hour 56 minutes

--- app/bot.py
+++ app/bot.py
@@ -38,21 +38,65 @@ configs = MongoClient('mongodb://mongo:27017').instaimg.configs
 
 
 def update_last_activity(chat_id: int):
+    """Update last user activity date in MongoDB"""
     user_query = {'_id': chat_id}
     configs.update_one(user_query, {'$set': {'last-activity': datetime.now()}})
 
 
-def start(update: Update, context: CallbackContext) -> None:
+def start(update: Update, context: CallbackContext) -> None:  # pylint: disable=unused-argument
     """Welcome message"""
     update.message.reply_text('Добро пожаловать в Text2Image бот.\n'
                               'Пришли мне текст и я переведу его в изображения.\n\n'
                               'Так же можно настроить шрифт и его размер:\n'
                               '/font — выбор шрифта\n'
-                              '/size — выбор размера шрифта\n',
-                              '/orientation  форма изображения')
+                              '/size — выбор размера шрифта\n'
+                              '/orientation — форма изображения')
     update_last_activity(update.effective_chat.id)
 
-def button(update: Update, context: CallbackContext) -> None:
+
+def parse_font_button(data):
+    """Parse data returned by font family selection"""
+    if data == 'font_roboto':
+        return {'font-family': 'roboto'}, 'Roboto'
+    if data == 'font_raleway':
+        return {'font-family': 'raleway'}, 'Raleway'
+    if data == 'font_playfair':
+        return {'font-family': 'playfair'}, 'Playfair'
+
+    return {'font-family': DEFAULT_FONT_FAMILY}, DEFAULT_FONT_FAMILY.title()
+
+
+def parse_font_size_button(data):
+    """Parse data returned by font size selection"""
+    if data == 'size_smallest':
+        return {'font-size': 20}, 'XS'
+    if data == 'size_small':
+        return {'font-size': 30}, 'S'
+    if data == 'size_medium':
+        return {'font-size': 40}, 'M'
+    if data == 'size_big':
+        return {'font-size': 50}, 'L'
+    if data == 'size_biggest':
+        return {'font-size': 60}, 'XL'
+
+    return {'font-size': DEFAULT_FONT_SIZE}, DEFAULT_FONT_SIZE
+
+
+def parse_orientation_button(data):
+    """Parse data returned by image orientation selection"""
+    if data == 'orientation_square':
+        return {'orientation': 'square'}, 'квадратная'
+    if data == 'orientation_vertical':
+        return {'orientation': 'vertical'}, 'вертикальная'
+    if data == 'orientation_horizontal':
+        return {'orientation': 'horizontal'}, 'горизонтальная'
+    if data == 'orientation_stories':
+        return {'orientation': 'stories'}, 'сториз'
+
+    return {'orientation': DEFAULT_ORIENTATION}, 'квадратная'
+
+
+def button(update: Update, context: CallbackContext) -> None:  # pylint: disable=unused-argument
     """Button press"""
     query = update.callback_query
     query.answer()
@@ -60,70 +104,26 @@ def button(update: Update, context: CallbackContext) -> None:
     user_query = {'_id': update.effective_chat.id}
 
     if query.data.startswith('font'):
-        if query.data == 'font_roboto':
-            set_query = {'font-family': 'roboto'}
-            selected_font = 'Roboto'
-        elif query.data == 'font_raleway':
-            set_query = {'font-family': 'raleway'}
-            selected_font = 'Raleway'
-        elif query.data == 'font_playfair':
-            set_query = {'font-family': 'playfair'}
-            selected_font = 'Playfair'
-        else:
-            set_query = {'font-family': DEFAULT_FONT_FAMILY}
-            selected_font = DEFAULT_FONT_FAMILY.title()
-
+        set_query, selected_font = parse_font_button(query.data)
         configs.update_one(user_query, {'$set': set_query})
         query.edit_message_text(text=f'Выбранный шрифт: {selected_font}')
         return
 
     if query.data.startswith('size'):
-        if query.data == 'size_smallest':
-            set_query = {'font-size': 20}
-            selected_size = 'XS'
-        elif query.data == 'size_small':
-            set_query = {'font-size': 30}
-            selected_size = 'S'
-        elif query.data == 'size_medium':
-            set_query = {'font-size': 40}
-            selected_size = 'M'
-        elif query.data == 'size_big':
-            set_query = {'font-size': 50}
-            selected_size = 'L'
-        elif query.data == 'size_biggest':
-            set_query = {'font-size': 60}
-            selected_size = 'XL'
-        else:
-            set_query = {'font-size': DEFAULT_FONT_SIZE}
-            selected_size = DEFAULT_FONT_SIZE
-
+        set_query, selected_size = parse_font_size_button(query.data)
         configs.update_one(user_query, {'$set': set_query})
         query.edit_message_text(text=f'Выбранный размер шрифта: {selected_size}')
         return
 
     if query.data.startswith('orientation'):
-        if query.data == 'orientation_square':
-            set_query = {'orientation': 'square'}
-            selected_orientation = 'квадратная'
-        elif query.data == 'orientation_vertical':
-            set_query = {'orientation': 'vertical'}
-            selected_orientation = 'вертикальная'
-        elif query.data == 'orientation_horizontal':
-            set_query = {'orientation': 'horizontal'}
-            selected_orientation = 'горизонтальная'
-        elif query.data == 'orientation_stories':
-            set_query = {'orientation': 'stories'}
-            selected_orientation = 'сториз'
-        else:
-            set_query = {'orientation': DEFAULT_ORIENTATION}
-            selected_orientation = 'квадратная'
-
+        set_query, selected_orientation = parse_orientation_button(query.data)
         configs.update_one(user_query, {'$set': set_query})
         query.edit_message_text(text=f'Выбранная форма изображения: {selected_orientation}')
 
     update_last_activity(update.effective_chat.id)
 
-def help_command(update: Update, context: CallbackContext) -> None:
+
+def help_command(update: Update, context: CallbackContext) -> None:  # pylint: disable=unused-argument
     """/help command"""
     update.message.reply_text('/font — выбор шрифта\n'
                               '/size — выбор размера шрифта')
@@ -131,7 +131,7 @@ def help_command(update: Update, context: CallbackContext) -> None:
     update_last_activity(update.effective_chat.id)
 
 
-def font_command(update: Update, context: CallbackContext) -> None:
+def font_command(update: Update, context: CallbackContext) -> None:  # pylint: disable=unused-argument
     """/font command"""
     font_list = [[InlineKeyboardButton(text='Roboto', callback_data='font_roboto'),
                   InlineKeyboardButton(text='Raleway', callback_data='font_raleway'),
@@ -145,7 +145,7 @@ def font_command(update: Update, context: CallbackContext) -> None:
     update_last_activity(update.effective_chat.id)
 
 
-def size_command(update: Update, context: CallbackContext) -> None:
+def size_command(update: Update, context: CallbackContext) -> None:  # pylint: disable=unused-argument
     """/size command"""
     font_list = [[InlineKeyboardButton(text='XS', callback_data='size_smallest'),
                   InlineKeyboardButton(text='S', callback_data='size_small'),
@@ -161,7 +161,7 @@ def size_command(update: Update, context: CallbackContext) -> None:
     update_last_activity(update.effective_chat.id)
 
 
-def orientation_command(update: Update, context: CallbackContext) -> None:
+def orientation_command(update: Update, context: CallbackContext) -> None:  # pylint: disable=unused-argument
     """/orientation command"""
     font_list = [[InlineKeyboardButton(text='Square', callback_data='orientation_square'),
                   InlineKeyboardButton(text='Vertical', callback_data='orientation_vertical'),
@@ -176,7 +176,7 @@ def orientation_command(update: Update, context: CallbackContext) -> None:
     update_last_activity(update.effective_chat.id)
 
 
-def response(update: Update, context: CallbackContext) -> None:
+def response(update: Update, context: CallbackContext) -> None:  # pylint: disable=unused-argument
     """Response with images"""
     user_config = configs.find_one({'_id': update.effective_chat.id})
     if not user_config:
